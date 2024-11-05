669e628b9545e7ca7b956b850c015a5f80845dcc
nirmalfernando <fernandonirmal607@gmail.com>
2024-11-05 20:17:07 +0530

    Created the ORM configuration

---
+++ backend/config/ormconfig.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+require('dotenv').config();
+
+module.exports = {
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    logging: process.env.DB_LOGGING === 'true',
+    entities: ["backend/models/*.js"],
+}
