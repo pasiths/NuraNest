85a3712e96b2f1489693d39cf2bf17d5091351f8
nirmalfernando <fernandonirmal607@gmail.com>
2024-11-05 21:49:11 +0530

    solved an issue where one .env is not set correctly

--- backend/config/ormconfig.js
+++ backend/config/ormconfig.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 module.exports = {
     type: process.env.DB_TYPE,
     host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    synchronize: process.env.DB_SYNC === 'true',
     logging: process.env.DB_LOGGING === 'true',
-    entities: ["backend/models/*.js"],
+    entities: ["backend/models/**/*.js"],
 }
