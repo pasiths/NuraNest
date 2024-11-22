f5271b496136b1f7117512c0aa31c6faeb19ac26
nirmalfernando <fernandonirmal607@gmail.com>
2024-11-22 10:50:35 +0530

    Created doctor routes

---
+++ backend/routes/doctorRoutes.js
@@ -0,0 +1,27 @@
+const {
+  createDoctorProfile,
+  getDoctorProfile,
+  updateDoctorProfile,
+  getAppointments,
+  updateAppointmentStatus,
+} = require("../controllers/doctor.js");
+const express = require("express");
+
+const router = express.Router();
+
+// Create the doctor's profile
+router.post("/profile", createDoctorProfile);
+
+// Get the doctor's profile
+router.get("/profile", getDoctorProfile);
+
+// Update the doctor's profile
+router.put("/profile", updateDoctorProfile);
+
+// Get all appointments of the doctor
+router.get("/appointments", getAppointments);
+
+// Update the status of an appointment
+router.put("/appointments/:appointmentId", updateAppointmentStatus);
+
+module.exports = router;
