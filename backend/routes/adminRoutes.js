4861ccb951cb06b9e853fa33605f028891502fe9
nirmalfernando <fernandonirmal607@gmail.com>
2024-11-22 10:15:25 +0530

    Updated admin routes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -6,6 +6,13 @@ const {
   getActiveAppointments,
   getCompletedAppointments,
   getCancelledAppointments,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+  getPayments,
+  getSuccessfulPayments,
+  getFailedPayments,
+  getPendingPayments,
 } = require("../controllers/admin.js");
 const express = require("express");
 
@@ -31,5 +38,26 @@ router.get("/appointments/completed", getCompletedAppointments);
 
 // Get all cancelled appointments
 router.get("/appointments/cancelled", getCancelledAppointments);
+
+// Create a blog
+router.post("/blogs", createBlog);
+
+// Update a blog
+router.put("/blogs/:id", updateBlog);
+
+// Delete a blog
+router.delete("/blogs/:id", deleteBlog);
+
+// Get all payments
+router.get("/payments", getPayments);
+
+// Get all successful payments
+router.get("/payments/successful", getSuccessfulPayments);
+
+// Get all failed payments
+router.get("/payments/failed", getFailedPayments);
+
+// Get all pending payments
+router.get("/payments/pending", getPendingPayments);
 
 module.exports = router;
